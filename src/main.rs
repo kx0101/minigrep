668a56c124249d9f1739692cfcb0f10c8c188611
Elijah Koulaxis <liakos.koulaxis@yahoo.com>
2023-11-26 23:56:05 +0200

    reading file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::env::args;
+use std::{env::args, fs};
 
 fn main() {
     let args: Vec<String> = args().collect();
 
-    for arg in args.iter() {
-        println!("{}", arg);
-    }
+    let query = &args[1];
+    let file = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file);
+
+    let contents = fs::read_to_string(file).expect("Failed to read file");
+
+    println!("With text:\n {}", contents);
 }
